21bc08f63db0a89d07156d084f3b7f8c28f3e34c
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-02-06 15:38:33 +0800

    Update sb.py

--- sb.py
+++ sb.py
@@ -6,7 +6,7 @@ from googletrans import Translator
 
 
 #client = LineClient()
-client = LineClient(id='rootmelo92118@gmail.com', passwd='melo92118')
+client = LineClient(id='mm7466256@gmail.com', passwd='asd-8502')
 #client = LineClient(authToken='AUTH TOKEN')
 client.log("Auth Token : " + str(client.authToken))
 
